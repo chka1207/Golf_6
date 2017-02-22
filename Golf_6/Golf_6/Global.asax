28a44123ec68c480f2fef29d930404c800cc7a31
Christoffer Karlsson <chka1207@student.miun.se>
2017-02-22 11:44:29 +0100

    Grund

---
+++ Golf_6/Golf_6/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Golf_6.MvcApplication" Language="C#" %>
---
+++ Golf_6/Golf_6/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Golf_6.Startup))]
+namespace Golf_6
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
