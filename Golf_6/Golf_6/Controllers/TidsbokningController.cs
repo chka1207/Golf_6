3289bca7b7709e54618ad2cff16f30d095bd6905
Christoffer Karlsson <chka1207@student.miun.se>
2017-02-27 15:29:00 +0100

    Controller + view för tidsbokning

---
+++ Golf_6/Golf_6/Controllers/TidsbokningController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Golf_6.Controllers
+{
+    public class TidsbokningController : Controller
+    {
+        // GET: Tidsbokning
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Tidsbokning/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Tidsbokning/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tidsbokning/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Tidsbokning/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Tidsbokning/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Tidsbokning/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Tidsbokning/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
