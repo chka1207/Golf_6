060cea15c307825278ba5d1d74194a319d061244
Christoffer Karlsson <chka1207@student.miun.se>
2017-02-27 14:46:08 +0100

    Ny modell

---
+++ Golf_6/Golf_6/Models/Tidsbokning.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Golf_6.Models
+{
+    public class Tidsbokning
+    {
+        public int BokningsID { get; set; }
+
+    }
+}
