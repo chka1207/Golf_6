43271cc2afa9a7e3e36b3f2e74974a9440e6b321
bjornnordsvahn <bjorn@nordsvahn>
2017-02-28 11:26:15 +0100

    Lagt till paket för Npgsql samt skapat modell för Postgres anslutning.

---
+++ Golf_6/Golf_6/Models/Postgres.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using Npgsql;
+
+namespace Golf_6.Models
+{
+    public class Postgres
+    {
+        private NpgsqlConnection _conn;
+        private NpgsqlConnection _cmd;
+        private NpgsqlConnection _dr;
+
+        public Postgres()
+        {
+            _conn = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+            try
+            {
+                _conn.Open();
+            }
+            catch
+            {
+                
+            }
+        }
+        
+    }
+}
