633b55caf5d9519eec975835585dab577f851e99
bjornnordsvahn <bjorn@nordsvahn>
2017-02-28 14:02:04 +0100

    Lagt till förslag på exempel metoder för hantering av SQL i Postgres.cs

--- Golf_6/Golf_6/Models/Postgres.cs
+++ Golf_6/Golf_6/Models/Postgres.cs
@@ -11,11 +11,11 @@ namespace Golf_6.Models
     public class Postgres
     {
         private NpgsqlConnection _conn;
-        private NpgsqlConnection _cmd;
-        private NpgsqlConnection _dr;
+        private NpgsqlCommand _cmd;
+        private NpgsqlDataReader _dr;
         private string _error;
 
-
+        //Metod för anslutning
         public Postgres()
         {
             _conn = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
@@ -23,11 +23,51 @@ namespace Golf_6.Models
             {
                 _conn.Open();
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 _error = ex.Message;
             }
         }
-        
+        //ExempelMetod för sqlFråga utan paramterar
+        private NpgsqlDataReader sqlFraga(string sql)
+        {
+            try
+            {
+                _cmd = new NpgsqlCommand(sql, _conn);
+                _dr = _cmd.ExecuteReader();
+                return _dr;
+            }
+            catch (NpgsqlException ex)
+            {
+                _error = ex.Message;
+                return null;
+            }
+        }
+        //ExempelMetod för sqlFråga som tar emot paramterar
+        public NpgsqlDataReader sqlFragaParams(string sql, params int[] p)
+        {
+            try
+            {
+                _cmd = new NpgsqlCommand(sql, _conn);
+
+                for (int i = 0; i < p.Length; i++)
+                {
+                    _cmd.Parameters.Add(new NpgsqlParameter("@p" + i, p[i]));
+                }
+
+                _dr = _cmd.ExecuteReader();
+
+                return _dr;
+            }
+
+            catch (NpgsqlException ex)
+            {
+                _error = ex.Message;
+
+                return null;
+            }
+
+        }
+
     }
 }
