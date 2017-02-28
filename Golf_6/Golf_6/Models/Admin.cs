28f09a31e0fc5256b6c8ed619975a5c6aca0ca1d
Toni Tuohisaari <tuttizaari@hotmail.com>
2017-02-28 12:08:03 +0100

    Basic adminvy

---
+++ Golf_6/Golf_6/Models/Admin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Golf_6.Models
+{
+    public class Admin
+    {
+        public string Fornamn { get; set; }
+        public string Efternamn { get; set; }
+        public string Adress { get; set; }
+        public string Postnummer { get; set; }
+        public string Ort { get; set; }
+        public string Kon { get; set; }
+        //public double Hcp { get; set; }
+        public int MedlemsKategori { get; set; }
+    }
+}
