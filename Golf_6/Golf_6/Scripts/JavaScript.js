b2c7b5a23a4677f75e2bdcf04f30b06fccf48f2c
Toni Tuohisaari <tuttizaari@hotmail.com>
2017-03-04 17:45:29 +0100

    Admin: Modal öppnas vid klick på medlem

--- Golf_6/Golf_6/Scripts/JavaScript.js
+++ Golf_6/Golf_6/Scripts/JavaScript.js
@@ -6,7 +6,28 @@ $(document).ready(function () {
     $('#table_id').DataTable();
     
 });
+
 $(document).ready(function () { //Datatablen som visar alla medlemmar i Admin/RedigeraMedlem
-    $('#alla_medlemmar').DataTable();
+    $('#alla_medlemmar').DataTable({
+        responsive: {
+            details: {
+                display: $.fn.dataTable.Responsive.display.modal({
+                    header: function (row) {
+                        var data = row.data();
+                        return 'Utökade detaljer för ' + data[0] + ' ' + data[1];
+                    }
+                }),
+                renderer: $.fn.dataTable.Responsive.renderer.tableAll({
+                    tableClass: 'table'
+                })
+            }
+        }
+    });
 });
+
+//$(document).ready(function () {
+//    $('#alla_medlemmar').DataTable();
+//});
+
+
 
