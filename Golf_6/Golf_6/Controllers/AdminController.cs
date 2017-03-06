892ffb140eef6696f080f109726c29744b2eea75
Toni Tuohisaari <tuttizaari@hotmail.com>
2017-03-06 09:41:02 +0100

    Kolumnnamnen ändrade i AllaMedlemmar

--- Golf_6/Golf_6/Controllers/AdminController.cs
+++ Golf_6/Golf_6/Controllers/AdminController.cs
@@ -29,7 +29,19 @@ namespace Golf_6.Controllers
             DataTable dt = new DataTable("MyTable");
            
             dt = medlemmarna.HämtaMedlemmar();
-           
+
+            dt.Columns[0].ColumnName = "Förnamn";
+            dt.Columns[1].ColumnName = "Efternamn";
+            dt.Columns[2].ColumnName = "Adress";
+            dt.Columns[3].ColumnName = "Postnummer";
+            dt.Columns[4].ColumnName = "Ort";
+            dt.Columns[5].ColumnName = "E-mail";
+            dt.Columns[6].ColumnName = "Kön";
+            dt.Columns[7].ColumnName = "Handikapp";
+            dt.Columns[8].ColumnName = "Medlemskategori";
+            dt.Columns[9].ColumnName = "GolfID";
+            dt.Columns[10].ColumnName = "Tele";
+
             return View(dt);
         }
         #endregion
